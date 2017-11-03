36b8d4008311bd921f469ed39778fb23aa540884
Sowmyasundari Nellore <sn478@afsconnect1.njit.edu>
2017-11-03 00:45:12 -0400

    this code is to diplay requirmnet2 records

--- index.php
+++ index.php
@@ -1,17 +1,34 @@
-
 <?php
+
+
+
 $dsn = 'mysql:dbname=sn478;host=sql1.njit.edu';
 $user = 'sn478';
 $password = 'RJRNIIe0';
 
 try {
     $dbh = new PDO($dsn, $user, $password);
-     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo 'Connected successfully<br>';
-} catch (PDOException $e) {
-  echo 'Connection failed: ' . $e->getMessage() . '<br>';
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $statement = $dbh->prepare("SELECT * FROM accounts WHERE id < 6"); 
+    $statement->execute();
+
+    // set the resulting array to associative
+    $result = $statement->setFetchMode(PDO::FETCH_ASSOC); 
+    $array = new RecursiveArrayIterator($statement->fetchAll());
+    foreach(new table($array) as $k=>$v) { 
+        echo $v .'<br>';
+        
+    }
 }
+catch(PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
+$dhp = null;
 
+class table extends RecursiveIteratorIterator { 
+   function __construct($it) { 
+        parent::__construct($it, self::LEAVES_ONLY); 
+    }
+} 
 
-
-?>
+?> 
